8af9fa32960a6f9f341c3d1b668dc24b816a4d1d
Gabriel Garcia <gabrielgarciaprog@gmail.com>
2022-08-21 13:12:04 -0300

    configuração e inserção dos padrões do tema para o template do figma de exemplo a ser seguido

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-
 import './styles/global.scss';
 
 function App() {
 
   return (
-    <div className="flex justify-center">
-      <h1 className="font-bold text-2xl text-blue-900">
-        React and Tailwind with Vitejs!
-      </h1>
-    </div>
+    <>
+    
+    </>
   )
 }
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
+    colors: {
+      red: {
+        200: "#f3a1a1",
+        DEFAULT: "#ea5959",
+        700: "#e32020",
+      },
+    },
+    fontFamily: {
+      sans: ["Lato", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
